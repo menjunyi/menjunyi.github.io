e4f5d9ec97009c78db85d74f6f3d91d67702fbed
menjunyi <junyimen@gmail.com>
2023-03-22 17:41:48 +1100

    init

---
+++ layouts/navbar.js
@@ -0,0 +1,20 @@
+
+const sections = document.querySelectorAll('[data-include]')
+const navLi = document.querySelectorAll('nav ul li')
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(pageYOffset >= (sectionTop - sectionHeight/3)){
+            current = section.getAttribute('data-include');
+        }
+    })
+
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        if(li.classList.contains(current)){
+            li.classList.add('active')
+        }
+    })
+})
